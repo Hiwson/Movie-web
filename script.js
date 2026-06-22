c159358ce98a849c514ce55fc74c5227b817ecbd
Hiwson <uliwillz001@gmail.com>
2026-06-22 16:52:21 +0300

    add css and js

---
+++ script.js
@@ -0,0 +1,14 @@
+//Js code
+let trailer_btn = document.getElementById('trailer');
+let watch_btn = document.getElementById('watch');
+let list_btn = document.getElementById('wish-list');
+
+/*trailer_btn.addEventListener(
+    "click", () => {
+        //alert("Page still under development")
+        location.href = "coming-soon.html";
+    }
+);*/
+function soon(){
+    location.href = "coming-soon.html";
+}
